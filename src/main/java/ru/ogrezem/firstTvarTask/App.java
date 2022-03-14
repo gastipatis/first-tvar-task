48819d9ab71e5e03d7fb7a94755a0af5155730f7
ogrezem <ogrezem@gmail.com>
2022-03-14 16:31:20 +0300

    Solved the task

--- src/main/java/ru/ogrezem/firstTvarTask/App.java
+++ src/main/java/ru/ogrezem/firstTvarTask/App.java
@@ -1,13 +1,49 @@
 package ru.ogrezem.firstTvarTask;
 
+import java.util.Scanner;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+
 /**
- * Hello world!
+ *
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+  public static void main(String[] args) {
+    int taskResult = doTask(readN());
+    System.out.printf("Task result: %d%n", taskResult);
+  }
+
+  private static int readN() {
+    var scanner = new Scanner(System.in);
+    while (true) {
+      System.out.print("Write the n value. It must be integer number: ");
+      try {
+        return scanner.nextInt();
+      } catch(NoSuchElementException ex) {
+        System.err.println("Error. N can be only integer number.");
+        scanner.nextLine();
+        continue;
+      } catch(Exception ex) {
+        ex.printStackTrace();
+        System.exit(1);
+      }
     }
+  }
+
+  private static int doTask(int n) {
+    if (n < 5) {
+      return 1;
+    }
+    for (int k = 2;; k++) {
+      int fivePowK = 1;
+      for (int i = 0; i < k; i++) {
+        fivePowK *= 5;
+      }
+      if (fivePowK > n) {
+        return k;
+      }
+    }
+  }
 }
